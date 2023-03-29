c244ef52989d1441c7604edf3de328d20b058e9d
tnccc <irei.frontend@gmail.com>
2023-03-29 23:23:07 +0900

    feat: GlobalHeaderを作成する

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import GlobalHeader from '@/components/GlobalHeader.vue'
+</script>
 
 <template>
+  <GlobalHeader />
   <main />
 </template>
 
--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -35,18 +35,100 @@ const lists: {id: string, section: string, link: string}[] = [
 
 <template>
   <header :class="$style.header">
-    <ul :class="$style.list">
-      <li 
-        v-for="item in lists"
-        :key="item.id"
-        :class="$style.item"
-      >
-        <a :href="item.link">
-          {{ item.section }}
-        </a>
-      </li>
-    </ul>
+    <h1 :class="$style.logo">
+      <a href="/">LOGO</a>
+    </h1>
+    <button :class="$style.hamburger">
+      <span 
+        v-for="i in 3"
+        :key="i"
+      />
+    </button>
+    <nav :class="$style.nav">
+      <ul :class="$style.list">
+        <li 
+          v-for="item in lists"
+          :key="item.id"
+          :class="$style.item"
+        >
+          <a :href="item.link">
+            {{ item.section }}
+          </a>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
-<style lang="scss" module></style>
+<style lang="scss" module>
+@use '@/assets/scss/mixin' as *;
+
+.header {
+  position: fixed;
+  top     : calc(var(--bv) * 10);
+  left    : calc(var(--bv) * 5);
+
+  .logo {
+    letter-spacing: var(--letter-spacing-medium);
+  }
+
+  .hamburger {
+    display: none;
+
+    @include mediaScreen('tablet') {
+      margin-block-start: calc(var(--bv) * 2);
+      display           : flex;
+      width             : calc(var(--bv) * 2.75);
+      height            : calc(var(--bv) * 2.75);
+      gap               : calc(var(--bv) / 2);
+      justify-content   : center;
+      flex-direction    : column;
+
+      span {
+        display         : block;
+        width           : 100%;
+        height          : 1px;
+        background-color: var(--main-color);
+        cursor          : pointer;
+        transition      : all .6s ease-out;
+
+        &:nth-of-type(2) {
+          width: calc(var(--bv) * 2);
+        }
+
+        &:nth-of-type(3) {
+          width: calc(var(--bv) * 1.25);
+        }
+      }
+    }
+  }
+
+  .nav {
+    margin-block-start: calc(var(--bv) * 5);
+
+    .list {
+
+      li + li {
+        padding-top: var(--bv);
+      }
+      .item {
+
+        a {
+          display   : block;
+          font-size : var(--font-size-min);
+          transition: color .3s;
+
+          &:hover {
+            color: var(--accent-color);
+          }
+        }
+      }
+    }
+
+    @include mediaScreen('tablet') {
+      opacity   : 0;
+      visibility: hidden;
+    }
+  }
+}
+</style>
